0c0484fb76b03a8cf5b8be7bd7a2824328e06172
MozanielJR <mozaniel.junior@a.ucb.br>
2022-11-06 11:32:57 -0300

    commit exercicio 2

---
+++ lista1exercicio2.c
@@ -0,0 +1,20 @@
+/*
+Faça um algoritmo que imprima o poema abaixo,com a mesma formatação:
+Caderno de poesias é um belo lugar.
+Tantas coisas lindas que eu gostaria de falar.
+Eu falo em forma de versos para todos poderem escutar.
+Agora você já sabe por que os poetas passam os dias escrevendo em seus cadernos de poesias.
+
+*/
+
+#include<stdio.h>
+
+int main(){
+
+printf("Caderno de poesias eh um belo lugar. \n");
+printf("Tantas coisas lindas que eu gostaria de falar. \n");
+printf("Eu falo em forma de versos para todos poderem escutar. \n");
+printf("Agora vocee ja sabe por que os poetas passam os dias escrevendo em seus cadernos de poesias. \n");
+
+
+}
--- teste.c
+++
@@ -1,2 +0,0 @@
-//teste do comit para o github
-
