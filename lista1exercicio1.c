192c2e57671e2916c4bb67bd2a3c56a3458f78c8
MozanielJR <mozaniel.junior@a.ucb.br>
2022-11-06 11:15:44 -0300

    commit exercicio 1

---
+++ lista1exercicio1.c
@@ -0,0 +1,13 @@
+/*
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!".
+ Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+*/
+
+#include<stdio.h>
+
+int main(){
+
+printf("Hello World! \n");
+
+
+}
